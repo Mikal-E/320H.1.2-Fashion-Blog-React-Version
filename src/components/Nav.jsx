67c7bf485570d973aaf3c225e0e198fbc19fd6e8
Mikal-E <melamin101@gmail.com>
2026-07-16 17:30:01 -0400

    Updated Nav.jsx function Nav() to accept props, in order to make it reusable for the footer nav.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,26 @@
-function Nav() {
+/* Added props to function Nav() to reuse it for the footer. */
+
+function Nav({ links, className }) {
 
   return (
 
     <nav className="nav-background" aria-label="Main Navigation" role="navigation">
+
+        <ul>
+
+            {links.map((link) => (
+
+                <li key={link.label}><a href={link.href}>{link.label}</a></li>
+
+            )
+
+            )}
+
+        </ul>
+
+    </nav>
+
+/*    <nav className="nav-background" aria-label="Main Navigation" role="navigation">
 
         <ul>
 
@@ -16,6 +34,7 @@ function Nav() {
         </ul>
 
     </nav>
+*/
 
   )
 
